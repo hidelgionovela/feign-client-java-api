a358ad6af71e9b74d4a76907ac77a3fed6bec5d5
hidelgionovela <hidelgionovela@gmail.com>
2023-11-23 11:17:50 +0200

    Initial commit

---
+++ src/main/java/com/hdlg9la/feignclientjavaapi/FeignClientJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.hdlg9la.feignclientjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientJavaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdlg9la/feignclientjavaapi/FeignClientJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdlg9la.feignclientjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
