6def4278a9f19a060f7ea3ecd8ea80827979c451
hidelgionovela <hidelgionovela@gmail.com>
2023-11-24 14:29:25 +0200

    chore: enable feign clients

--- src/main/java/com/hdlg9la/feignclientjavaapi/FeignClientJavaApiApplication.java
+++ src/main/java/com/hdlg9la/feignclientjavaapi/FeignClientJavaApiApplication.java
@@ -2,8 +2,10 @@ package com.hdlg9la.feignclientjavaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // habilitando o uso do FeignClients
 public class FeignClientJavaApiApplication {
 
     public static void main(String[] args) {
